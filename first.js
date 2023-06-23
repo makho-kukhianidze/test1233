83ebad691ad5831a21c15b5b77133339cee39002
makho-kukhianidze <94704279+makho-kukhianidze@users.noreply.github.com>
2023-06-24 00:56:26 +0400

    Add files via upload

--- first.js
+++ first.js
@@ -3,10 +3,10 @@ fetch('data.json')
   .then(data => {
     // JSON data is loaded and parsed, you can work with it here
     console.log(data);
-    
+
     // Get the <p> element
     var jsonDataElement = document.getElementById("json-data");
-    
+
     // Set the JSON data as the text content of the <p> element
     jsonDataElement.textContent = JSON.stringify(data);
   })
