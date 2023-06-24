24644b6a3776c4200c5edce56eacc9ef4af3fd52
makho-kukhianidze <94704279+makho-kukhianidze@users.noreply.github.com>
2023-06-24 11:23:12 +0400

    Add files via upload

--- first.js
+++ first.js
@@ -1,71 +1,74 @@
- fetch('data.json')
- .then(response => response.json())
- .then(data => {
+    // Place the merged JavaScript code here
+    fetch('data.json')
+      .then(response => response.json())
+      .then(data => {
+        // JSON data is loaded and parsed, you can work with it here
+        console.log(data);
 
-   console.log(data);
+        // Access the "data" array from the JSON
+        const dataArray = data.data;
 
+        // Get the <div> element for JSON data
+        const jsonDataElement = document.getElementById("json-data");
 
-   const dataArray = data.data;
+        // Check if the element exists before setting its content
+        if (jsonDataElement) {
+          // Clear the existing content of the <div> element
+          jsonDataElement.innerHTML = "";
 
-   // Get the <div> element for JSON data
-   const jsonDataElement = document.getElementById("json-data");
+          // Iterate over each row in the data array
+          dataArray.forEach(row => {
+            // Create a new <p> element for each row
+            const paragraph = document.createElement("p");
+            paragraph.textContent = row.join(", ");
 
-   // Check if the element exists before setting its content
-   if (jsonDataElement) {
-     // Clear the existing content of the <div> element
-     jsonDataElement.innerHTML = "";
+            // Append the <p> element to the <div> element
+            jsonDataElement.appendChild(paragraph);
+          });
+        } else {
+          console.error("Element with ID 'json-data' not found.");
+        }
 
-     // Iterate over each row in the data array
-     dataArray.forEach(row => {
-       // Create a new <p> element for each row
-       const paragraph = document.createElement("p");
-       paragraph.textContent = row.join(", ");
+        // Create the table
+        function createTable(data) {
+          var table = document.createElement("table");
+          var thead = document.createElement("thead");
+          var tbody = document.createElement("tbody");
 
-       // Append the <p> element to the <div> element
-       jsonDataElement.appendChild(paragraph);
-     });
-   } else {
-     console.error("Element with ID 'json-data' not found.");
-   }
+          // Create table header
+          var headerRow = document.createElement("tr");
+          // Change the header names here
+          var headerNames = ["Name", "Age", "Location"]; // Example header names
+          headerNames.forEach(function(headerName) {
+            var th = document.createElement("th");
+            th.textContent = headerName;
+            headerRow.appendChild(th);
+          });
+          thead.appendChild(headerRow);
 
-   // Create the table
-   function createTable(data) {
-     var table = document.createElement("table");
-     var thead = document.createElement("thead");
-     var tbody = document.createElement("tbody");
+          // Create table rows
+          data.forEach(function(obj) {
+            var row = document.createElement("tr");
+            Object.keys(obj).forEach(function(key) {
+              var cell = document.createElement("td");
+              cell.textContent = obj[key];
+              row.appendChild(cell);
+            });
+            tbody.appendChild(row);
+          });
 
-     // Create table header
-     var headerRow = document.createElement("tr");
-     Object.keys(data[0]).forEach(function(key) {
-       var th = document.createElement("th");
-       th.textContent = key;
-       headerRow.appendChild(th);
-     });
-     thead.appendChild(headerRow);
+          // Append thead and tbody to the table
+          table.appendChild(thead);
+          table.appendChild(tbody);
 
-     // Create table rows
-     data.forEach(function(obj) {
-       var row = document.createElement("tr");
-       Object.keys(obj).forEach(function(key) {
-         var cell = document.createElement("td");
-         cell.textContent = obj[key];
-         row.appendChild(cell);
-       });
-       tbody.appendChild(row);
-     });
+          return table;
+        }
 
-     // Append thead and tbody to the table
-     table.appendChild(thead);
-     table.appendChild(tbody);
-
-     return table;
-   }
-
-   // Get the container element and append the table
-   var tableContainer = document.getElementById("tableContainer");
-   tableContainer.appendChild(createTable(dataArray));
- })
- .catch(error => {
-   // Handle any error that occurred during the fetch
-   console.error('Error:', error);
- });
+        // Get the container element and append the table
+        var tableContainer = document.getElementById("tableContainer");
+        tableContainer.appendChild(createTable(dataArray));
+      })
+      .catch(error => {
+        // Handle any error that occurred during the fetch
+        console.error('Error:', error);
+      });
